7b18ed5fef3fea558c5c4f9a6f8fe9d71f3d4554
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2025-02-25 19:52:09 +0600

    update origin file

--- src/app.ts
+++ src/app.ts
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    // origin: true,
 
-    // origin: 'https://woofspot.net',
+    origin: 'https://woofspot.net',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }),
